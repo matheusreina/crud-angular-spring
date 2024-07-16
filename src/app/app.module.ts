d9985d83b9709760b190b03fc913981c0bca5d55
matheusreina <matheushfreina@gmail.com>
2024-07-16 11:53:58 -0300

    Chamada HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
-  providers: [provideClientHydration(), provideAnimationsAsync()],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
+  // courses: Observable<Course[]>;
+  courses: Course[] = [];
 
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    // this.courses = this.coursesService.listAll();
+
+    this.coursesService
+      .listAll()
+      .subscribe((courses) => (this.courses = courses));
+  }
 }
